4e01c681fb77bb905daa4b1ec0a44daca20f4d6d
Ernane Sena <ernane.sena@gmail.com>
2012-09-26 14:23:12 -0300

    Update sphinx capistrano

--- config/recipes/sphinx.rb
+++ config/recipes/sphinx.rb
@@ -1,6 +1,9 @@
-set_default(:sphinx_pid)      { "#{current_path}/tmp/pids/searchd.development.pid" }
-set_default(:sphinx_query_log){ "#{shared_path}/log/searchd.query.log" }
-set_default(:sphinx_log)      { "#{shared_path}/log/searchd.log" }
+set_default(:sphinx_pid)         { "#{current_path}/tmp/pids/searchd.development.pid" }
+set_default(:sphinx_query_log)   { "#{shared_path}/log/searchd.query.log" }
+set_default(:sphinx_log)         { "#{shared_path}/log/searchd.log" }
+set_default(:sphinx_searchd_file){ "#{shared_path}/config/db/sphinx" }
+before 'deploy:update_code', 'thinking_sphinx:stop'
+after 'deploy:update_code', 'thinking_sphinx:start'
 
 namespace :sphinx do
   desc "Install Sphinx-Search"
@@ -12,16 +15,22 @@ namespace :sphinx do
   desc "Generate the production.sphinx.conf configuration file."
   task :setup, roles: :app do
     run "mkdir -p #{shared_path}/config/db/sphinx"
-    template "production.sphinx.conf.erb", "#{shared_path}/config/production.sphinx.conf"
-    template "app_config.yml.erb", "#{shared_path}/config/app_config.yml"
+    #template "production.sphinx.conf.erb", "#{shared_path}/config/production.sphinx.conf"
+    template "sphinx.yml.erb", "#{shared_path}/config/sphinx.yml"
   end
   after "deploy:setup", "sphinx:setup"
+
+  desc "Symlink Sphinx indexes"
+  task :symlink_indexes, :roles => [:app] do
+    run "ln -nfs #{shared_path}/db/sphinx #{release_path}/db/sphinx"
+  end
+  after 'deploy:finalize_update', 'sphinx:symlink_indexes'
 
 
   desc "Symlink the database.yml file into latest release"
   task :symlink, roles: :app do
-    run "ln -nfs #{shared_path}/config/app_config.yml #{release_path}/config/app_config.yml"
-    run "ln -nfs #{shared_path}/config/production.sphinx.conf #{release_path}/config/production.sphinx.conf"
+    run "ln -nfs #{shared_path}/config/sphinx.yml #{release_path}/config/sphinx.yml"
+    #run "ln -nfs #{shared_path}/config/production.sphinx.conf #{release_path}/config/production.sphinx.conf"
   end
   after "deploy:finalize_update", "sphinx:symlink"
   
