6f0a5d260203b2e973283367759e9e79cf652eea
Ernane Sena <ernane.sena@gmail.com>
2013-01-20 22:59:32 -0300

    Update

--- app/workers/answer_work.rb
+++ app/workers/answer_work.rb
@@ -1,6 +1,5 @@
 class AnswerWork
   include Sidekiq::Worker
-  sidekiq_options :queue => :crawler, :timeout => 60, :retry => true, :backtrace => true
 
   def perform(id, count)
     answer = Answer.find(id)
