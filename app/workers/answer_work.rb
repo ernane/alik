4d55c6fb81797190c1a9c565b80da7d63cf2a50b
Ernane Sena <ernane.sena@gmail.com>
2013-01-20 22:52:16 -0300

    Add arguments sidekiq

--- app/workers/answer_work.rb
+++ app/workers/answer_work.rb
@@ -1,5 +1,6 @@
 class AnswerWork
   include Sidekiq::Worker
+  sidekiq_options :queue => :crawler, :timeout => 60, :retry => true, :backtrace => true
 
   def perform(id, count)
     answer = Answer.find(id)
